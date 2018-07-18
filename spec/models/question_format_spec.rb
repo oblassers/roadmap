759f9cd85a5728b127d802fa222a36d767c2c94b
Bodacious <gavin@katanacode.com>
2018-07-18 10:49:14 +0100

    Fix format of model validation specs

--- spec/models/question_format_spec.rb
+++ spec/models/question_format_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe QuestionFormat, type: :model do
 
-  describe "validations" do
+  context "validations" do
 
     it { is_expected.to validate_presence_of(:title) }
 
